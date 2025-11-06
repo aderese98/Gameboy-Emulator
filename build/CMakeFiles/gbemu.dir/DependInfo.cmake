d8930de8dfbccb6c8cedd345458cb9072dc59c86
aderese98 <dev.code.abe@gmail.com>
2025-11-06 10:23:48 -0500

    Added the Joypad!

---
+++ build/CMakeFiles/gbemu.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ad-dev/Documents/C++/Gameboy/src/bus.cpp" "CMakeFiles/gbemu.dir/src/bus.cpp.o" "gcc" "CMakeFiles/gbemu.dir/src/bus.cpp.o.d"
+  "/home/ad-dev/Documents/C++/Gameboy/src/cpu.cpp" "CMakeFiles/gbemu.dir/src/cpu.cpp.o" "gcc" "CMakeFiles/gbemu.dir/src/cpu.cpp.o.d"
+  "/home/ad-dev/Documents/C++/Gameboy/src/joypad.cpp" "CMakeFiles/gbemu.dir/src/joypad.cpp.o" "gcc" "CMakeFiles/gbemu.dir/src/joypad.cpp.o.d"
+  "/home/ad-dev/Documents/C++/Gameboy/src/main.cpp" "CMakeFiles/gbemu.dir/src/main.cpp.o" "gcc" "CMakeFiles/gbemu.dir/src/main.cpp.o.d"
+  "/home/ad-dev/Documents/C++/Gameboy/src/ppu.cpp" "CMakeFiles/gbemu.dir/src/ppu.cpp.o" "gcc" "CMakeFiles/gbemu.dir/src/ppu.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/gbemu.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gbemu.dir/src/bus.cpp.o"
+  "CMakeFiles/gbemu.dir/src/bus.cpp.o.d"
+  "CMakeFiles/gbemu.dir/src/cpu.cpp.o"
+  "CMakeFiles/gbemu.dir/src/cpu.cpp.o.d"
+  "CMakeFiles/gbemu.dir/src/joypad.cpp.o"
+  "CMakeFiles/gbemu.dir/src/joypad.cpp.o.d"
+  "CMakeFiles/gbemu.dir/src/main.cpp.o"
+  "CMakeFiles/gbemu.dir/src/main.cpp.o.d"
+  "CMakeFiles/gbemu.dir/src/ppu.cpp.o"
+  "CMakeFiles/gbemu.dir/src/ppu.cpp.o.d"
+  "gbemu"
+  "gbemu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gbemu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
